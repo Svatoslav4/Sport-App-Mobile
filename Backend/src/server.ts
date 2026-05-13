18b1e934285df6b7c6d5726a41edbc13a1643df7
Svatoslav4 <ksvatoslav2006@gmail.com>
2026-05-13 17:15:40 +0300

    update code in file

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -2,8 +2,9 @@ import dotenv from 'dotenv'
 import app from './app'
 
 dotenv.config()
-const port = process.env.PORT || 5000
 
-app.listen(port,() => {
-    console.log(`Server runnig on port http://localhost:${port}`)
+const port = Number(process.env.PORT)|| 5000
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${port}`)
 })
